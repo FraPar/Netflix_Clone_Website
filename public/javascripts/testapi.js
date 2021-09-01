f11ee8549c9eeeabf8629e66dab16d4917939ee2
Fra Par <francois.parenti@gmail.com>
2021-09-01 10:34:03 +0200

    Added Cards for Films and Jquery associated

--- public/javascripts/testapi.js
+++ public/javascripts/testapi.js
@@ -1,13 +1,15 @@
 $(function() {
-    appendInDiv = '.high_rated .card_list';
-    dataToAppend = '<i class="fas fa-arrow-left"></i>'
-    $( appendInDiv ).append( dataToAppend )
-    dataToAppend = '<div class="film_card"><p>IMAGE</p></div>';
 
-    var rating = 9.4
-    getMostRated(rating)
-
-
+    var nbDiv = $(".high_rated .card_list .film_card").length;
+    console.log(nbDiv)
+    getMostRated();
+    var filmGenre = "comedy";
+    getByGenre(filmGenre);
+    // while (nbDiv < 10 || rating != 9) {
+        // getMostRated(rating)
+        // rating -= 0.1
+        // console.log(rating)
+    // }
 
     // var nbDiv = $(".high_rated .card_list .film_card").length;
     // console.log(nbDiv)
@@ -19,26 +21,62 @@ $(function() {
 });
 
 var callBackInitializeSuccess = function(data) {
+    appendInDiv = '.high_rated .card_list';
+    dataToAppend = '<i class="fas fa-arrow-left"></i>'
+    $( appendInDiv ).append( dataToAppend )
+
+    
     console.log(" ");
     console.log("Donnée api", data)
     console.log("Count", data.count)
     console.log(" ");
-    for (i = 0; i < data.count; i++) {
+    for (i = 0; i < 5; i++) {
         console.log("Id du film : " + data.results[i].id);
         console.log("Titre du film : " + data.results[i].title);
         console.log("Genre du film : " + data.results[i].genres);
+        console.log("Image Url : " + data.results[i].image_url);
+        console.log("URL : " + data.results[i].url);
+        dataToAppend = '<div class="film_card"><a href="' + data.results[i].url + '"><img src=' + data.results[i].image_url + ' alt="Image du film"></a></div>';
+        $( appendInDiv ).append( dataToAppend )
         console.log(" ");
     }
+    dataToAppend = '<i class="fas fa-arrow-right"></i>'
+    $( appendInDiv ).append( dataToAppend )
+}
+
+
+var callBackCategory = function(data) {
+    appendInDiv = '#category_1 .card_list';
+    dataToAppend = '<i class="fas fa-arrow-left"></i>'
+    $( appendInDiv ).append( dataToAppend )
+
+    
+    console.log(" ");
+    console.log("Donnée api", data)
+    console.log("Count", data.count)
+    console.log(" ");
+    for (i = 0; i < 5; i++) {
+        console.log("Id du film : " + data.results[i].id);
+        console.log("Titre du film : " + data.results[i].title);
+        console.log("Genre du film : " + data.results[i].genres);
+        console.log("Image Url : " + data.results[i].image_url);
+        console.log("URL : " + data.results[i].url);
+        dataToAppend = '<div class="film_card"><a href="' + data.results[i].url + '"><img src=' + data.results[i].image_url + ' alt="Image du film"></a></div>';
+        $( appendInDiv ).append( dataToAppend )
+        console.log(" ");
+    }
+    dataToAppend = '<i class="fas fa-arrow-right"></i>'
+    $( appendInDiv ).append( dataToAppend )
 }
 
 
 var callBackGetSuccess = function(data) {
     console.log("Donnée api", data)
-    console.log("Genre du film : " + data.results[0].name)
-    console.log("Genre du film : " + data.results[1].name)
-    console.log("Genre du film : " + data.results[2].name)
-    console.log("Genre du film : " + data.results[3].name)
-    console.log("Genre du film : " + data.results[4].name)
+    // console.log("Genre du film : " + data.results[0].name)
+    // console.log("Genre du film : " + data.results[1].name)
+    // console.log("Genre du film : " + data.results[2].name)
+    // console.log("Genre du film : " + data.results[3].name)
+    // console.log("Genre du film : " + data.results[4].name)
 
     var element = document.getElementById("resultat_api")
     element.innerHTML = "Le genre du film est " + data.results[0].name;
@@ -63,12 +101,27 @@ function buttonClickGet() {
 }
 
 
-function getMostRated(getRating) {
-    var url = "http://localhost:8000/api/v1/titles/?imdb_score="+getRating
+function getMostRated() {
+    var url = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score"
     console.log(url)
     $.get(url, callBackInitializeSuccess).done(function() {
         //alert( "second success" )
     })
+     .fail(function() {
+         alert( "error" );
+     })
+     .always(function() {
+         //alert( "finished" )
+     });
+
+}
+
+function getByGenre(filmGenre) {
+    var url = "http://localhost:8000/api/v1/titles/?genre_contains=" + filmGenre;
+    console.log(url)
+    $.get(url, callBackCategory).done(function() {
+        //alert( "second success" )
+     })
      .fail(function() {
          alert( "error" );
      })
