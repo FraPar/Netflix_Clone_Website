b8cdd40a62bdc7ca9cb79f08569272f4692b76ad
Fra Par <francois.parenti@gmail.com>
2021-09-06 11:17:41 +0200

    Finished Website

--- public/javascripts/movies.js
+++ public/javascripts/movies.js
@@ -1,28 +1,248 @@
-$( () => {
-    let search = $('#search')
-    let sendLike = $('#likeButton')
-    let sendComment = $('#commentButton')
-    let commentValue = $('#locationComment')
-    let validatePost = $('.validationButton')
+$(function() {
+    var nbDiv = $(".high_rated .card_list .film_card").length;
+    console.log(nbDiv)
+    console.log(thisIsTheDiv)
+    getMostRatedFilms();
 
-    // fonction appelée au début et apres le search
-    // recupere tableau de classe (get element by className)
-    // forEach sur élément récupéré
-    // pour chacun, appel ajax avec ID élément courant (pour nb like et commentaire en '.length')
+    for (category = 1; category <= 3; category++) {
+        if (category == 1) {
+            filmGenre = "action";
+        } else if (category == 2) {
+            filmGenre = "comedy";
+        } else {
+            filmGenre = "adventure";
+        }
+        thisIsTheDiv = "category_"+category;
+        getByGenre(filmGenre, thisIsTheDiv);
+    }
 
-    //Permet la recherche d'un nouveau film de la catégorie
-    validatePost.each(function() {
-        var card = this;
-        card.addEventListener('click', function() {
-            $.ajax({
-                url:`http://localhost:8000/api/v1/titles/`,
-                method: 'GET',
-                success: function(response, status, status_msg) {
-                    console.log(response.message);
-                },
-                error: function(err, error_msg, error_status) {
-                    console.log(err);
-                }
+});
+
+$(document).ready(function() {
+
+        /* OVERLAY MODULE */
+    const closeModalButtons = document.querySelectorAll('[data-close-button]')
+    const overlay = document.getElementById('overlay')
+    
+    overlay.addEventListener('click', () => {
+            const modals = document.querySelectorAll('.modal.active')
+            modals.forEach(modal => {
+                closeModal(modal)
             })
+    })
+
+    closeModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modal = button.closest('.modal');
+            closeModal(modal);
         })
     })
+
+        /* FILM DETAIL MODULE */
+    $("body").on('click', ".film_card", function(event) {
+        let idFilm = event.currentTarget.querySelector("div").innerText
+        const modal = document.querySelector('.modal');
+        initializingDescription = false;
+        getFilmById(idFilm, initializingDescription)
+        openModal(modal)
+    });
+
+        /* MAJ SELECTION DE FILMS */
+    $("body").on('click', ".fa-arrow-right, .fa-arrow-left", function(event) {
+        let urlPage = event.currentTarget.querySelector("div").innerText
+        //let thisDiv = event.currentTarget.parentElement.parentElement.id
+        getThisDiv(event)
+        getSelection(urlPage)
+    });
+
+});
+
+var filmGenre = "";
+var thisIsTheDiv = "";
+var filmDescription = "";
+
+var callBackPreviousPage = function(data) {
+    appendInDiv = '#category_1 .card_list';
+    $( appendInDiv ).empty();
+    if (data.previous != null) {
+        dataToAppend = '<i class="fas fa-arrow-left"><div class="previous_page">\
+        <p>'+ data.previous +'</p></div></i></p>'
+        $( appendInDiv ).append( dataToAppend )
+    }
+
+    for (i = 0; i < 5; i++) {
+        dataToAppend = '<div class="film_card">\
+                        <img src=' + data.results[i].image_url + ' alt="Image du film">\
+                        <div id="idFilm"> ' + data.results[i].id + ' </div>\
+                        </div>';
+        $( appendInDiv ).append( dataToAppend )
+    }
+    dataToAppend = '<i class="fas fa-arrow-right"><div class="next_page">\
+                    <p>'+ data.next +'</p></div></i></p>'
+
+    $( appendInDiv ).append( dataToAppend )
+}
+
+var callBackInitializeSuccess = function(data) {
+    console.log("data callBackInitializeSuccess")
+    console.log(data)
+    let idFilm = data.results[0].id;
+    console.log("idFilm callBackInitializeSuccess")
+    console.log(idFilm)
+    initializingDescription = true;
+    console.log(initializingDescription)
+    getFilmById(idFilm, initializingDescription)
+    console.log("filmDescription callBackInitializeSuccess")
+    console.log(filmDescription)
+    appendInDiv = '#gondola_head .film_card';
+    dataToAppend = '<h1>' + data.results[0].title + '</h1>\
+                    <div id="idFilm"> ' + data.results[0].id + ' </div>'
+    $( appendInDiv ).append( dataToAppend )
+    document.getElementById("gondola_head").setAttribute("style", 'background-image:url('+data.results[0].image_url+');\
+                                                                   background-size: 20em 30em;\
+                                                                   background-position: center;\
+                                                                   background-position-x: 19%;\
+                                                                   background-repeat: no-repeat;');
+
+    //document.getElementById("gondola_head").style.background-image = "url(data.results[0].image_url);
+    /*background-image:url(images/image_de_fond.png);*/
+    thisIsTheDiv = "high_rated"
+    loadSelection(data, thisIsTheDiv)
+}
+
+var callBackFilmId = function(data) {
+    appendInDiv = '.film_details';
+    $( appendInDiv ).empty();
+    filmDescription = data.description
+    dataToAppend = `<div class="popUp">
+                    <img src="`+ data.image_url + `" alt="Image du film">
+                    <h1>`+ data.title + `</h1>\
+                    " "\
+                    <p><strong>Genre complet du film :</strong> `+ data.genres + `</p>\
+                    <p><strong>Date de sortie :</strong> `+ data.year + `</p>\
+                    <p><strong>Rated :</strong> `+ data.rated + `</p>\
+                    <p><strong>Score IMDB :</strong> `+ data.imdb_score + `</p>\
+                    <p><strong>Réalisateur :</strong> `+ data.writers + `</p>\
+                    <p><strong>Liste des acteurs :</strong> `+ data.actors + `</p>\
+                    <p><strong>Durée :</strong> `+ data.duration + `</p>\
+                    <p><strong>Pays d'origine :</strong> `+ data.countries + `</p>\
+                    <p><strong>Résultat au Box Office :</strong> `+ data.reviews_from_critics + `</p>\
+                    <p><strong>Résumé du film :</strong> `+ data.description + `</p>\
+                    </div>`;
+    $( appendInDiv ).append( dataToAppend )
+    console.log("filmDescription callBackFilmId")
+    console.log(filmDescription)
+}
+
+function getMostRatedFilms() {
+    var url = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score"
+    $.get(url, callBackInitializeSuccess).done(function() {
+        //alert( "second success" )
+    })
+     .fail(function() {
+         alert( "error" );
+     })
+     .always(function() {
+         //alert( "finished" )
+     });
+
+}
+
+function getByGenre(filmGenre, thisIsTheDiv) {
+    var url = "http://localhost:8000/api/v1/titles/?genre_contains=" + filmGenre;
+    $.get(url).done(function(data) {
+        //alert( "second success" )
+        loadSelection(data, thisIsTheDiv)
+     })
+     .fail(function() {
+         alert( "error" );
+     })
+     .always(function() {
+         //alert( "finished" )
+     });
+}
+
+function getFilmById(idFilm, initializingDescription) {
+    var url = "http://localhost:8000/api/v1/titles/" + idFilm;
+    $.get(url, callBackFilmId).done(function(data) {
+        //alert( "second success" )
+        if (initializingDescription == true) {
+            console.log("Ca viens de début")
+            appendInDiv = '#gondola_head .film_card';
+            dataToAppend = `<p><strong>Résumé du film :</strong> `+ data.long_description + `</p>\
+                            </div>`;
+            $( appendInDiv ).append( dataToAppend )
+        }
+     })
+     .fail(function() {
+         alert( "error" );
+     })
+     .always(function() {
+         //alert( "finished" )
+     });
+
+}
+
+function getSelection(urlPage) {
+    var url = urlPage;
+    $.get(url).done(function(data) {
+        //alert( "second success" )
+        loadSelection(data, thisIsTheDiv)
+     })
+     .fail(function() {
+         alert( "error" );
+     })
+     .always(function() {
+         //alert( "finished" )
+     }); 
+}
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+function getThisDiv(event) {
+    let thisDiv = event.currentTarget.parentElement.parentElement.id
+    thisIsTheDiv = thisDiv
+}
+
+function loadSelection(data, thisIsTheDiv) {
+    appendInDiv = '#'+ thisIsTheDiv +' .card_list';
+    $( appendInDiv ).empty();
+    if (data.previous != null) {
+        dataToAppend = '<i class="fas fa-arrow-left"><div class="previous_page">\
+        <p>'+ data.previous +'</p></div></i>'
+        $( appendInDiv ).append( dataToAppend )
+    } else {
+        dataToAppend = '<i class="fas fa-arrow-circle-left"><div class="previous_page">\
+        <p>'+ data.previous +'</p></div></i>'
+        $( appendInDiv ).append( dataToAppend )
+        
+    }
+
+    if (data.previous === null && thisIsTheDiv == "high_rated") {
+        i = 1
+    } else {
+        i = 0
+    }
+
+    for (i;  i < 5; i++) {
+        dataToAppend = '<div class="film_card">\
+                        <img src=' + data.results[i].image_url + ' alt="Image du film">\
+                        <div id="idFilm"> ' + data.results[i].id + ' </div>\
+                        </div>';
+        $( appendInDiv ).append( dataToAppend )
+    }
+    dataToAppend = '<i class="fas fa-arrow-right"><div class="next_page">\
+                    <p>'+ data.next +'</p></div></i></p>'
+
+    $( appendInDiv ).append( dataToAppend )
+}
